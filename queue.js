38f6f8afb760b80f9568e4e6ef27c86aa6dbb228
mellowcholy <114334493+mellowcholy@users.noreply.github.com>
2025-10-07 21:38:27 +1100

    queue pops

--- queue.js
+++ queue.js
@@ -4,7 +4,7 @@ module.exports = {
 	run(client) {
 		// setup queue
 		client.queue = new Collection();
-		client.queue.set("Top", new Array());
+		client.queue.set("Top", new Array(1, 2, 3));
 		client.queue.set("Jungle", new Array());
 		client.queue.set("Mid", new Array());
 		client.queue.set("Bot", new Array());
@@ -26,11 +26,28 @@ module.exports = {
 			}).then(msg => client.latestInhousePost = msg);
 		};
 
+		function CheckQueuePop() {
+			const queue = client.queue;
+
+			let count = 0;
+			for (const key of queue.keys()) {
+				currentQueue = queue.get(key);
+
+				const val = key == "Fill" ? currentQueue.length : Math.min(2, currentQueue.length);
+				count += val;
+			}
+
+			if (count == 10) {
+				console.log("Queue pop!");
+			}
+		}
+
 		// create join queue func
 		client.JoinQueue = function(userId, position) {
 			const queue = client.queue;
 			const queuePos = queue.get(position);
 
+			/* TODO: RE-ENABLE THIS
 			for (const key of queue.keys()) {
 				const currentQueue = queue.get(key);
 				const isUserId = (v) => v == userId;
@@ -40,9 +57,11 @@ module.exports = {
 				if (key == position) { return 0; } // you are already queing for this role
 
 				currentQueue.splice(found, 1);
-			}
+			}*/
 
 			queuePos.push(userId);
+
+			CheckQueuePop();
 			return 1;
 		};
 	},
