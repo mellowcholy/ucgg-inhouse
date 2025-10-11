905a80b6d6ef591400716d1dc0438b713e4e5256
mellowcholy <114334493+mellowcholy@users.noreply.github.com>
2025-10-11 16:48:02 +1100

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,10 +1,11 @@
 const { Events } = require("discord.js");
+const { clientId } = require('../configdev.json'); // DEV MODE
 
 module.exports = {
 	name: Events.MessageCreate,
 	execute(message) {
 		if (message.channelId != "1424956618361147432") { return; } // not inhouse channel
-		if (message.member.id == "1424621192819769396") { return; } // caitlyn sent the msg
+		if (message.member.id == clientId) { return; } // caitlyn sent the msg
 
 		const client = message.client;
 
