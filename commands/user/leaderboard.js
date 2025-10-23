a609b4273c5393f7b48b9908726d5ae633e5be05
mellowcholy <114334493+mellowcholy@users.noreply.github.com>
2025-10-23 21:45:34 +1100

    added leaderboradfdfs

--- commands/user/leaderboard.js
+++ commands/user/leaderboard.js
@@ -1,6 +1,4 @@
-const { SlashCommandBuilder, InteractionContextType, AttachmentBuilder } = require("discord.js");
-const Canvas = require('@napi-rs/canvas');
-const { request } = require('undici');
+const { SlashCommandBuilder, InteractionContextType, MessageFlags } = require("discord.js");
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -19,124 +17,15 @@ module.exports = {
 				{ name: "Support MMR", value: "Support" },
 
 			))
-		.addIntegerOption((option) => option.setName("pagenumber").setDescription("What page to view"))
 		.setContexts(InteractionContextType.Guild),
 	async execute(interaction) {
 		await interaction.deferReply();
 
 		const client = interaction.client;
-
-		// sort by key
-		const leaderboard = [];
-		for await (const [key, value] of client.keyv.iterator()) {
-			if (!value.wins) { continue; }
-
-			leaderboard.push([key, value]);
-    	};
-
 		const stat = interaction.options.getString("stat");
-		const key = stat;
-		let label = "";
-		let mmr = false;
+		const pageNum = 0;
 
-		switch (stat) {
-		case "wins":
-		case "losses":
-		case "mvps":
-			label = stat.charAt(0).toUpperCase() + stat.slice(1);
-			break;
-		case "points":
-			label = "Credits";
-			break;
-		case "Top":
-		case "Jungle":
-		case "Mid":
-		case "Bot":
-		case "Support":
-			label = stat;
-			mmr = true;
-			break;
-		}
-
-		const pageNum = interaction.options.getInteger("pagenumber") ?? 0;
-
-		const sorted = leaderboard.sort(([, a], [, b]) => mmr ? b["mmrs"][key] - a["mmrs"][key] : b[key] - a[key]);
-		const pages = [];
-
-		let page = [];
-		let counter = 1;
-		for (let i = 0; i < sorted.length; i++) {
-			page.push([i + 1, sorted[i]]);
-
-			if (counter == 6) {
-				pages.push(page);
-				page = [];
-				counter = 0;
-			}
-
-			counter++;
-		}
-
-		if (page.length > 0) { pages.push(page); }
-
-		// create leaderboard image
-		const canvas = Canvas.createCanvas(825, 620);
-		const context = canvas.getContext("2d");
-		const background = await Canvas.loadImage('./img/leaderboard.png');
-		const slot = await Canvas.loadImage('./img/leaderboard_slot.png');
-
-		context.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-		// title bar
-		context.font = 'bold 30px Bahnschrift';
-		context.fillStyle = '#1e1d1b';
-		context.textAlign = "center";
-		context.fillText(`In House - ${label}`, 413, 60);
-
-		async function drawUser(id, y, position, value) {
-			// background
-			context.drawImage(slot, 27, y);
-
-			context.font = '30px Bahnschrift';
-			context.fillStyle = '#1e1d1b';
-
-			// position
-			context.textAlign = "center";
-			context.fillText(`${position}.`, 69, y + 48);
-
-			// name
-			/*
-			let member = interaction.guild.members.cache.get(id);
-
-			if (!member) {
-				member = await interaction.guild.members.fetch(id);
-			}
-
-			const name = member.displayname;
-			*/
-			const name = id;
-			context.textAlign = "left";
-			context.fillText(`${name}`, 182, y + 48);
-
-			// value
-			context.textAlign = "right";
-			context.fillText(`${value}`, canvas.width - 57, y + 48);
-		};
-
-		let yPos = 92;
-		for (let i = 0; i < pages[pageNum].length; i++) {
-			const entry = pages[pageNum][i];
-			const position = entry[0];
-			const id = entry[1][0];
-			const value = mmr ? entry[1][1]["mmrs"][key] : entry[1][1][key];
-
-			await drawUser(id, yPos, position, value);
-
-			yPos += 3 + 82;
-		}
-
-		const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'leaderboard.png' });
-
-		await interaction.editReply({ files: [attachment] }).catch(console.error); return;
+		const panel = await client.panels.get("Leaderboard")(client, stat, pageNum, interaction);
+		await interaction.editReply({ components: [panel[1], panel[0]], flags: MessageFlags.IsComponentsV2, files: [panel[2]] }).catch(console.error); return;
 	},
 };
