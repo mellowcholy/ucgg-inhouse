6876488a3d55834e15821330c963f978c676453c
thefinalcoomer <114334493+mellowcholy@users.noreply.github.com>
2026-05-25 16:14:31 +1000

    add random quotes

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,25 @@
-const { Events } = require("discord.js");
+const { Events, ActivityType } = require("discord.js");
+
+randomQuotes = [
+	"👰🏻‍♀️ I see… you must be maidenless.",
+	"🤷🏻‍♀️ Who is it that will protect you from my ire?",
+	"📖 Shei-fuwan-kaunmay'hai-au",
+	"🤌🏻 I assure you: I am quite patient.",
+	"😱 AI'MOUTAI JAI YON!",
+	"🙀 AGE OF RETRIBUTION!",
+	"😿 This is a reward for my sacrifice.",
+	"😾I mustn't take a single breath for granted.",
+];
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+
+		client.randomQuotes = randomQuotes;
+
+		client.user.setActivity(client.randomQuotes[Math.floor(Math.random() * client.randomQuotes.length)], { type: ActivityType.Custom });
 
 		// run queue handling
 		const queue = require("../inhouse/queue.js");
